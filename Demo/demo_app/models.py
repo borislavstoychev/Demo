2598d49f3bff5f3b9c1c48411b4a2d60a79b96aa
borislavstoychev <stoy4ew@gmail.com>
2021-11-02 13:05:31 +0200

    added model field in models.py

--- Demo/demo_app/models.py
+++ Demo/demo_app/models.py
@@ -4,10 +4,10 @@ from django.db import models
 
 
 class Demo(models.Model):
-    title = models.CharField(max_length=25)
+    title = models.CharField(max_length=50)
     description = models.TextField()
     image_url = models.URLField()
     date = models.DateField()
-    category = models.CharField(max_length=25)
+    category = models.CharField(max_length=50)
     rating = models.PositiveIntegerField()
     author = models.CharField(max_length=50)
