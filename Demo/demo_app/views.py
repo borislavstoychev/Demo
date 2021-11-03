4f4ad12cafc3fab02dc470a187b1f4106fa721fc
borislavstoychev <stoy4ew@gmail.com>
2021-11-03 12:57:58 +0200

    added pagination

--- Demo/demo_app/views.py
+++ Demo/demo_app/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework import viewsets
 
+from Demo.demo_app.custom_pagination import CustomPagination
 from Demo.demo_app.models import Demo
 from Demo.demo_app.serializers import SerializerDemo
 
@@ -10,3 +11,4 @@ from Demo.demo_app.serializers import SerializerDemo
 class DemoView(viewsets.ModelViewSet):
     serializer_class = SerializerDemo
     queryset = Demo.objects.all()
+    pagination_class = CustomPagination
--- Demo/settings.py
+++ Demo/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 123)
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -135,5 +135,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_WHITELIST = [
      'http://localhost:3000'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
 
 django_heroku.settings(locals())
